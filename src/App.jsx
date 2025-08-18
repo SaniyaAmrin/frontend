3c0fb62f93c2cc9250e167250899f32ecc7e1273
SaniyaAmrin <2300030438@kluniversity.in>
2025-08-18 21:27:13 +0530

    V1.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/S219_CICD">
+    <Router basename="/frontend">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
